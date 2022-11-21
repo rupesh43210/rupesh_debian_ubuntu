6911e3715db264dfaf84e53e37daa31ffade8403
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2022-11-21 09:32:04 +0530

    Create aiofunctions.sh

---
+++ aiofunctions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#chcek is a user is root
+checksudo(){
+      checksudo{}{
+      if [[ $EUID -ne 0 ]]; then
+           echo "This script must be run as root" 
+           echo " You need to run this script as root - run the command sudo su"
+           exit 1
+        fi
+      }
+}
